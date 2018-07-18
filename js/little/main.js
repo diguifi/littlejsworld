35afc2b098649544e212bdd972de7d0e6eb7aed2
diguifi <lumia-diguifi@hotmail.com>
2018-07-18 09:14:47 -0300

    New map - Map 17 (new order)

--- js/little/main.js
+++ js/little/main.js
@@ -21,6 +21,7 @@ var Game = {
            new Maps.DeafultLevels(map14),
            new Maps.DeafultLevels(map15),
            new Maps.DeafultLevels(map16),
+           new Maps.DeafultLevels(map17),
            new Maps.More()],
 
     init: function init(){
